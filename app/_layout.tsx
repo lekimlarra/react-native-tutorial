a315feeab9310c5b73fa455e66129870d5a6dfd5
lekimlarra <33814049+lekimlarra@users.noreply.github.com>
2024-10-28 17:53:15 +0100

    ocultando el header en todas las pantallas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,11 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack screenOptions={{
+      headerShown: false, // Esto oculta el encabezado en todas las pantallas
+    }}>
+      <Stack.Screen name="index" options={{ }}/>
+      <Stack.Screen name="screens/ViewImageScreen" options={{ headerShown: false }}/>
     </Stack>
   );
 }
